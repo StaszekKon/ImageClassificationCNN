20f6ed799837c9296433cbee6ee456292281dbd4
Staszek <stanislaw.konczal@gmail.com>
2023-08-05 13:46:00 +0200

    Testing the model

--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 from flask import Flask, render_template, request
 from model import preprocess_img, predict_result
 import os
+from werkzeug.utils import secure_filename
 
 IMAGES_FOLDER = os.path.join('Images', 'seg_pred')
 # Instantiating flask app
@@ -13,19 +14,25 @@ app.config['IMAGES_FOLDER'] = IMAGES_FOLDER
 def main():
     full_filename = os.path.join(app.config['IMAGES_FOLDER'], '7110.jpg')
     img = preprocess_img(full_filename)
-    pred2 = predict_result(img)
+    recognition = predict_result(img)
 
-    return render_template("index.html", class_names = str(pred2), filename = full_filename)
+    return render_template("index.html", class_names = str(recognition), filename = full_filename)
 
 
 # Prediction route
-@app.route('/predict', methods=['POST'])
+@app.route('/predict', methods=['GET'])
 def predict_image_file():
     try:
-        if request.method == 'POST':
-            img = preprocess_img(request.files['file'].stream)
-            pred = predict_result(img)
-            return render_template("result.html", predictions=str(pred))
+        if request.method == 'GET':
+            # img = preprocess_img(request.files['file'].stream)
+            # full_filename = os.path.join(app.config['IMAGES_FOLDER'], '7322.jpg')
+            uploaded_img = request.files['uploaded-file']
+            img_filename = secure_filename(uploaded_img.filename)
+            # uploaded_img.save(os.path.join(app.config['IMAGES_FOLDER'], img_filename))
+            # img = preprocess_img(img_filename)
+            # pred = predict_result(img)
+            testS = "Ala"
+            return render_template("result.html", predictions="pred", name2 = testS, img2 = img_filename)
 
 
     except:
